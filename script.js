73986e9ffac971a3d9c926543b1cc763aa0e9bea
Joel Rudin <jarud55@gmail.com>
2020-01-26 13:00:51 -0500

    fixed confirm option

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  
+
 }
 
 // Add event listener to generate button
@@ -16,123 +16,84 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function writePassword() {
- 
+
   var psswrdlngth = prompt("How many characters long would you like your password")
-  if(psswrdlngth  < 8 || psswrdlngth > 128) {
+  if (psswrdlngth < 8 || psswrdlngth > 128) {
     alert("Your password should be between 8 and 128 characters long.")
   } else {
- var lwerChar = confirm("Would you like lowercase letters?"); 
- var uppChar =  confirm("Would you like uppercase letters?"); 
- var spclChar = confirm("Would you like special characters?");
- var numerChar = confirm("Woud you like numeric characters?");   
+    var lwerChar = confirm("Would you like lowercase letters?");
+    var uppChar = confirm("Would you like uppercase letters?");
+    var spclChar = confirm("Would you like special characters?");
+    var numerChar = confirm("Woud you like numeric characters?");
   }
-  
- 
- 
- var alph = 'abcdefghijklmnopqrstuvwxyz';
- //Arrays
- //generate uppercase array
- var alphUpr = alph.toUpperCase().split('').reverse();
 
- //special characters array
- var spclCHARS =  ["!","#","$","%","&","()","*", "+","-",".","/",":",";","<","=",">","?","@","[","^","`","{","|","}","~","]"];
- 
- //generate lowercase array
- var alphArr = alph.split('')
- 
- //generate number array:
- var nums = [];
- for (var i = 0; i <= 128; i++) {
-   nums.push(i);
- }
-console.log(nums)
- var randNum = function() {
-  return Math.floor(Math.random()*20)
-}
-console.log("THis is randNum :" + randNum() + " This is randNUm " + randNum())
- 
-   if(alphArr) {
-     randNum < alphArr.length
-   }
-   if(alphUpr) {
-    randNum < alphUpr.length
-  } 
-   if(spclCHARS) {
-     randNum < spclCHARS.length  
-   }  if(nums) {
-     randNum < nums.length   
-   }
+  var alph = 'abcdefghijklmnopqrstuvwxyz';
+  //Arrays
+  //generate uppercase array
+  var alphUpr = alph.toUpperCase().split('').reverse();
 
-var passwordJoin = [];
-  
-var multiArr = [alphArr, alphUpr, spclCHARS, nums];
+  //special characters array
+  var spclCHARS = ["!", "#", "$", "%", "&", "()", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "`", "{", "|", "}", "~", "]"];
 
-console.log("This is : " + multiArr[0][-1])
+  //generate lowercase array
+  var alphArr = alph.split('')
 
-if(lwerChar) {
-  passwordJoin.push(multiArr[0][randNum()])
-} else if (lwerChar=false) {
-  passwordJoin.push(multiArr[-1])
-}
-if(uppChar) { 
-  passwordJoin.push(multiArr[1][randNum()]) 
-} else if (uppChar=false) {
-  passwordJoin.push(multiArr[-1])
-}
-if(spclChar) {
-  passwordJoin.push(multiArr[2][randNum()])
-} else if (spclChar=false) {
-  passwordJoin.push(multiArr[-1])
-}if (nums) {
-  passwordJoin.push(multiArr[3][randNum()])
-    } else if (nums=false) {
-      passwordJoin.push(multiArr[-1])
-    }   
+  //generate number array:
+  var nums = [];
+  for (var i = 0; i <= 128; i++) {
+    nums.push(i);
+  }
+
+  //generate random numbers
+  console.log(nums)
+  var randNum = function () {
+    return Math.floor(Math.random() * 20)
+  }
   var randNum2 = function getRandomArbitrary(min, max) {
-        return Math.floor((Math.random() * (max - min) + min));
+    return Math.floor((Math.random() * (max - min) + min));
+  }
+  console.log("THis is randNum :" + randNum() + " This is randNUm " + randNum())
+
+  if (alphArr) {
+    randNum < alphArr.length
+  }
+  if (alphUpr) {
+    randNum < alphUpr.length
+  }
+  if (spclCHARS) {
+    randNum < spclCHARS.length
+  }
+  if (nums) {
+    randNum < nums.length
   }
 
-  while(passwordJoin.length < psswrdlngth) {
-    passwordJoin.push(multiArr[randNum2(0, 3)][randNum()])
-    
+  var passwordJoin = [];
+
+  var multiArr = [alphArr, alphUpr, spclCHARS, nums];
+
+  console.log("This is : " + multiArr[0][-1])
+
+
+
+
+  while (passwordJoin.length < psswrdlngth) {
+
+    if (lwerChar == false) {
+      passwordJoin.push(multiArr[randNum2(1, 3)][randNum()])
+    } else if (uppChar == false) {
+      passwordJoin.push(multiArr[randNum2(2, 3)][randNum()])
+
+    } else if (spclChar == false) {
+      passwordJoin.push(multiArr[randNum2(0, 1)][randNum()])
+
+    } else if (numerChar == false) {
+      passwordJoin.push(multiArr[randNum2(0, 2)][randNum()])
+    } else {
+      passwordJoin.push(multiArr[randNum2(0, 3)][randNum()])
+    }
   }
-  
+
 
   var getPassword = document.getElementById("password");
-    getPassword.innerHTML = passwordJoin.join(" ");
-  }
-
- 
-
-
-
-
-
-
-
-
-
-    
-    
-    
-  
-
-
-  
-
-  
-
- 
-  
-
-
-
-
-
-
-
-
-
-
-
-
+  getPassword.innerHTML = passwordJoin.join(" ");
+}
